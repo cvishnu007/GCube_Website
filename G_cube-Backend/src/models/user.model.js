4b9b5d309e1de5282620f0c962c548573c57ac1a
MrAstatine <akshatkrish2005@gmail.com>
2025-02-02 20:37:32 +0530

    improve user model

--- G_cube-Backend/src/models/user.model.js
+++ G_cube-Backend/src/models/user.model.js
@@ -4,26 +4,45 @@ const userSchema = new Schema({
     username:{
         type:String,
         trim:true,
-        index:true
+        index:true,
+        required:true
     },
     email:{
         type:String,
         trim:true,
+        required:true,
         index:true,
         unique:true
     },
     srn:{
         type:String,
         index:true,
+        required:true,
         unique:true,
-        trim:true
+        trim:true,
+        lowercase:true
     },
     answer:[
         {
             type:Schema.Types.ObjectId,
             ref:"Answer"
         }
-    ]
+    ],
+    branch:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    semester:{
+        type:Number,        
+        required:true,
+    },
+    contactNo:{
+        type:Number,
+        required:true,
+        unique:true
+    }
 },{
     timestamps:true
 })
