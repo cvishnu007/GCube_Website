cb6b7496d906998d1506b261d068f0c1898debe4
MrAstatine <akshatkrish2005@gmail.com>
2025-02-02 21:02:53 +0530

    user controller

--- G_cube-Backend/src/middlewares/multer.middleware.js
+++ G_cube-Backend/src/middlewares/multer.middleware.js
@@ -3,7 +3,6 @@ import multer from "multer";
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
         cb(null,'./public/temp');
-        //cb is callback.  cb(null,'./public/temp') means no error and the destination is ./public/temp
     },
     filename: function(req,file,cb){
         cb(null,file.originalname)
