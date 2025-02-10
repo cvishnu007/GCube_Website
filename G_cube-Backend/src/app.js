3ec529e3feb13dbdb9885b0db8be9e918809305d
cvishnu007 <141037435+cvishnu007@users.noreply.github.com>
2025-02-10 13:31:15 +0530

    Update app.js

--- G_cube-Backend/src/app.js
+++ G_cube-Backend/src/app.js
@@ -16,10 +16,14 @@ app.use(cookieParser()); //put for future use. not required now
     console.log('Request Headers:', req.headers);
     console.log('Request Body:', req.body);
     next();
-});     */
+});*/
 
 import queryRouter from "./routers/query.router.js";
+import answerRouter from "./routers/answer.router.js";
+import userRouter from "./routers/user.router.js"
 
+app.use("/api/v1/user",upload.none(),userRouter);
+app.use("/api/v1/answer",upload.none(),answerRouter);
 app.use("/api/v1/query",upload.none(), queryRouter);
 
 export { app };
